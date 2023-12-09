9f7bf6b938aae9fad7b1c4647355ed496f7fde7d
Nouras84 <hanana.nouras@gmail.com>
2023-12-09 18:51:30 +0100

    backend to test

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -1,46 +1,106 @@
-const Recipe = require("../models/Recipe"); // Adjust the path based on your project structure
+const Recipe = require("../models/Recipe");
 
 // Function to create a new recipe
 exports.createRecipe = async (req, res) => {
   try {
-    // Extract recipe data from req.body and create a new recipe
-    // Save the recipe to the database
+    const { title, ingredients, preparationSteps, categories, images } =
+      req.body;
+    const author = req.user.userId; // Assuming req.user is set in authMiddleware
 
-    res.status(201).json({ message: "Recipe created successfully" });
+    const newRecipe = new Recipe({
+      title,
+      ingredients,
+      preparationSteps,
+      author,
+      categories,
+      images,
+    });
+
+    await newRecipe.save();
+
+    res
+      .status(201)
+      .json({ message: "Recipe created successfully", recipe: newRecipe });
   } catch (error) {
-    res.status(500).json({ message: "Error creating recipe" });
+    res
+      .status(500)
+      .json({ message: "Error creating recipe", error: error.message });
   }
 };
 
 // Function to get a specific recipe
 exports.getRecipe = async (req, res) => {
   try {
-    // Fetch the recipe from the database using req.params.recipeId
+    const recipeId = req.params.recipeId;
 
-    res.json({ recipe: "Recipe data here" });
+    const recipe = await Recipe.findById(recipeId);
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    res.json({ recipe });
   } catch (error) {
-    res.status(500).json({ message: "Error fetching recipe" });
+    res
+      .status(500)
+      .json({ message: "Error fetching recipe", error: error.message });
   }
 };
 
 // Function to update a recipe
 exports.updateRecipe = async (req, res) => {
   try {
-    // Update the recipe in the database using req.params.recipeId and req.body
+    const recipeId = req.params.recipeId;
+    const updateData = req.body;
+    const userId = req.user.userId;
 
-    res.json({ message: "Recipe updated successfully" });
+    const recipe = await Recipe.findById(recipeId);
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    // Check if the user is the author of the recipe
+    if (recipe.author.toString() !== userId) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to edit this recipe" });
+    }
+
+    const updatedRecipe = await Recipe.findByIdAndUpdate(recipeId, updateData, {
+      new: true,
+    });
+
+    res.json({ message: "Recipe updated successfully", recipe: updatedRecipe });
   } catch (error) {
-    res.status(500).json({ message: "Error updating recipe" });
+    res
+      .status(500)
+      .json({ message: "Error updating recipe", error: error.message });
   }
 };
 
 // Function to delete a recipe
 exports.deleteRecipe = async (req, res) => {
   try {
-    // Delete the recipe from the database using req.params.recipeId
+    const recipeId = req.params.recipeId;
+    const userId = req.user.userId;
+
+    const recipe = await Recipe.findById(recipeId);
+    if (!recipe) {
+      return res.status(404).json({ message: "Recipe not found" });
+    }
+
+    // Check if the user is the author of the recipe
+    if (recipe.author.toString() !== userId) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to delete this recipe" });
+    }
+
+    await Recipe.findByIdAndDelete(recipeId);
 
     res.json({ message: "Recipe deleted successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Error deleting recipe" });
+    res
+      .status(500)
+      .json({ message: "Error deleting recipe", error: error.message });
   }
 };
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -15,3 +15,27 @@ const authMiddleware = (req, res, next) => {
 };
 
 module.exports = authMiddleware;
+
+// Improved Error Responses + Extract Bearer Token
+
+// const jwt = require("jsonwebtoken");
+
+// const authMiddleware = (req, res, next) => {
+//   try {
+//     const authHeader = req.headers["authorization"];
+//     if (!authHeader) return res.status(401).json({ message: "No token provided" });
+
+//     const token = authHeader.split(' ')[1]; // Extracting the token from "Bearer [token]"
+//     if (!token) return res.status(401).json({ message: "No token provided" });
+
+//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//       if (err) return res.status(401).json({ message: "Invalid token" });
+//       req.user = decoded; // Add the decoded user data to the request object
+//       next();
+//     });
+//   } catch (error) {
+//     res.status(401).json({ message: "Authentication failed", error: error.message });
+//   }
+// };
+
+// module.exports = authMiddleware;
